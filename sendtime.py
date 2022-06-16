485588b0b303dabcb1290aacf91e178572e5f5b1
jhhua <69577632+jhhua@users.noreply.github.com>
2022-06-16 23:51:07 +0800

    Create sendtime.py

---
+++ sendtime.py
@@ -0,0 +1,19 @@
+#coding:utf-8  
+import requests,json   
+from datetime import timedelta,datetime, date, time, timezone
+headers={
+     'Content-Type': 'application/json'} 
+webhook = 'https://oapi.dingtalk.com/robot/send?access_token=' 
+#定义要发送的数据
+mobile = '1008611'
+time = datetime.now()
+tm = str(time)
+data = {
+     
+    "msgtype": "text",
+    "text": {
+     "content": '!!!!!!nbimportant message\r\n' + 'success:'  +  "@" + mobile + '（!!）：\n' + tm +'\ncurrent time' + '            '},
+    "at": {
+     "atMobiles": "['"+ mobile + "']","isAtAll": False}
+}
+requests.post(webhook, data=json.dumps(data), headers=headers)
